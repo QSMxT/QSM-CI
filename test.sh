14282b45fc1f36ec92eb41a314c104cc5a43b5b3
Ashley Stewart <a.stewart.au@gmail.com>
2023-07-07 12:51:12 +1000

    Fix tests

--- test.sh
+++ test.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
 # install dependencies
+echo "[INFO] Downloading dependencies"
 pip install qsm-forward webdavclient3
 
 # download head-phantom-maps
+echo "[INFO] Downloading test data"
 python get-maps.py
 tar xf head-phantom-maps.tar
 rm head-phantom-maps.tar
 
 # generate bids data
+echo "[INFO] Simulating BIDS dataset"
 qsm-forward head-phantom-maps/ bids
 
