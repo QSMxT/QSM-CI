aa059adedc9f10c869b046d4818b0d85f8fb34ca
Ashley Stewart <a.stewart.au@gmail.com>
2023-09-11 11:09:08 +1000

    Install tree

--- test.sh
+++ test.sh
@@ -16,6 +16,9 @@ echo "[INFO] Downloading dependencies"
 pip install qsm-forward==0.11 webdavclient3
 export PATH=$PATH:/home/runnerx/.local/bin
 
+sudo apt-get update
+sudo apt-get install tree
+
 # download head-phantom-maps
 echo "[INFO] Downloading test data"
 python get-maps.py
