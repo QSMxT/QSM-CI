0c51c315b43776d81da14627ec56fef17e9d8af5
Steffen Bollmann <stebo85@users.noreply.github.com>
2023-10-13 14:06:28 +1000

    Upload images to object storage (#8)
    * upload and add output file to backend
    
    * with secrets
    
    * cleanup secrets
    
    * cleanup
    
    * refactor

--- upload.sh
+++ upload.sh
@@ -15,7 +15,8 @@ for file in `ls *.nii.gz`; do
     mv $file ${IMAGE_HASH}_$file
 
     # Upload to Nectar Swift Object Storage
-    if curl --output /dev/null --silent --head --fail "https://object-store.rc.nectar.org.au:8888/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/qsmxt/${IMAGE_HASH}_$file"; then
+    URL=https://object-store.rc.nectar.org.au:8888/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/qsmxt/${IMAGE_HASH}_$file
+    if curl --output /dev/null --silent --head --fail "${URL}"; then
         echo "[DEBUG] ${IMAGE_HASH}_$file exists in nectar swift object storage"
     else
         echo "[DEBUG] ${IMAGE_HASH}_$file does not exist yet in nectar swift - uploading it there as well!"
@@ -37,14 +38,24 @@ for file in `ls *.nii.gz`; do
         fi
 
         swift upload qsmxt ${IMAGE_HASH}_$file --segment-size 1073741824
+
+        # Check if it is uploaded to Nectar Swift Object Storage and if so, add it to the database
+        if curl --output /dev/null --silent --head --fail "${URL}"; then
+            echo "[DEBUG] ${IMAGE_HASH}_$file exists in nectar swift object storage"
+
+            curl -X POST \
+            -H "X-Parse-Application-Id: '"${PARSE_APPLICATION_ID}"'" \
+            -H "X-Parse-REST-API-Key: '"${PARSE_APPLICATION_ID}"'" \
+            -H "Content-Type: application/json" \
+            -d '{"url":"'"${URL}"'"}' \
+            https://parseapi.back4app.com/classes/Images
+
+        else
+            echo "[DEBUG] ${IMAGE_HASH}_$file does not exist yet in nectar swift"
+            exit 2
+        fi
     fi
 
-    # Check if it is uploaded to Nectar Swift Object Storage
-    if curl --output /dev/null --silent --head --fail "https://object-store.rc.nectar.org.au:8888/v1/AUTH_dead991e1fa847e3afcca2d3a7041f5d/qsmxt/${IMAGE_HASH}_$file"; then
-        echo "[DEBUG] ${IMAGE_HASH}_$file exists in nectar swift object storage"
-    else
-        echo "[DEBUG] ${IMAGE_HASH}_$file does not exist yet in nectar swift"
-        exit 2
-    fi
+
 
 done
