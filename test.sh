a0235e2d5b945facf5c6a93b65ecf25f961f3fa6
Ashley Stewart <a.stewart.au@gmail.com>
2023-10-13 12:01:24 +1000

    Algo name update

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ bash datasets/simple_phantom.sh
 # bash datasets/head_phantom.sh
 
 # run reconstruction
-bash algos/bet_romeo_vsharp_rts.sh
+bash algos/romeo_vsharp_rts.sh
 
 # metrics
 bash metrics/metrics.sh
