487c2c2a65e4273ffb60c034eb878990c2d92678
Ashley Stewart <a.stewart.au@gmail.com>
2023-10-27 16:00:06 +1000

    Add nextqsm

---
+++ algos/romeo_nextqsm.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+# create output directory
+mkdir -p "recons/${ALGO_NAME}"
+
+echo "[INFO] Pulling QSMxT image"
+sudo docker pull vnmd/qsmxt_6.2.0:20231012
+
+echo "[INFO] Creating QSMxT container"
+docker create --name qsmxt-container -it -v $(pwd):/tmp vnmd/qsmxt_6.2.0:20231012 /bin/bash
+
+echo "[INFO] Starting QSMxT container"
+docker start qsmxt-container
+
+echo "[INFO] Starting QSM reconstruction"
+docker exec qsmxt-container bash -c "qsmxt /tmp/bids/ /tmp/qsmxt_output --premade nextqsm -auto_yes --use_existing_masks"
+
+echo "[INFO] Collecting QSMxT results"
+sudo mv qsmxt_output/qsm/*.nii "recons/${ALGO_NAME}/${ALGO_NAME}.nii"
+
+echo "[INFO] Deleting old outputs"
+sudo rm -rf qsmxt_output/
+
+#end
