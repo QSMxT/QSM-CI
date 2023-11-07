483015ee4b1d2f25150cdadc2e762d952a3c2012
Ashley Stewart <a.stewart.au@gmail.com>
2023-11-08 06:40:17 +1000

    Only gzip .nii files

--- algos/romeo_vsharp_rts.sh
+++ algos/romeo_vsharp_rts.sh
@@ -18,7 +18,7 @@ echo "[INFO] Starting QSM reconstruction"
 docker exec qsmxt-container bash -c "qsmxt /tmp/bids/ /tmp/qsmxt_output --premade fast -auto_yes --use_existing_masks"
 
 echo "[INFO] Collecting QSMxT results"
-sudo gzip -f qsmxt_output/qsm/*.nii*
+sudo gzip -f qsmxt_output/qsm/*.nii
 ls qsmxt_output/qsm/
 sudo mv qsmxt_output/qsm/*.nii.gz "recons/${ALGO_NAME}/${ALGO_NAME}.nii.gz"
 
