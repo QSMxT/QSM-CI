83886787fe9b8ee984b5e50277bcaf012436a2a4
Ashley Stewart <a.stewart.au@gmail.com>
2023-11-08 05:07:21 +1000

    Update to use .gz

--- algos/romeo_vsharp_rts.sh
+++ algos/romeo_vsharp_rts.sh
@@ -18,7 +18,8 @@ echo "[INFO] Starting QSM reconstruction"
 docker exec qsmxt-container bash -c "qsmxt /tmp/bids/ /tmp/qsmxt_output --premade fast -auto_yes --use_existing_masks"
 
 echo "[INFO] Collecting QSMxT results"
-sudo mv qsmxt_output/qsm/*.nii "recons/${ALGO_NAME}/${ALGO_NAME}.nii"
+gzip qsmxt_output/qsm/*.nii
+sudo mv qsmxt_output/qsm/*.nii.gz "recons/${ALGO_NAME}/${ALGO_NAME}.nii.gz"
 
 echo "[INFO] Deleting old outputs"
 sudo rm -rf qsmxt_output/
