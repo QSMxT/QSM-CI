23ac172371090588bea170946e2e55ad56257b96
Ashley Stewart <a.stewart.au@gmail.com>
2024-01-02 09:30:07 +1000

    Added ROMEO-PDF-RTS pipeline

---
+++ algos/romeo_pdf_rts.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+# create output directory
+mkdir -p "recons/${ALGO_NAME}"
+
+echo "[INFO] Pulling QSMxT image"
+sudo docker pull vnmd/qsmxt_6.2.0:20231012
+
+echo "[INFO] Creating QSMxT container"
+docker create --name qsmxt-container -it -v $(pwd):/tmp vnmd/qsmxt_6.2.0:20231012 /bin/bash
+
+echo "[INFO] Starting QSMxT container"
+docker start qsmxt-container
+
+echo "[INFO] Starting QSM reconstruction"
+docker exec qsmxt-container bash -c "qsmxt /tmp/bids/ /tmp/qsmxt_output --premade bet --bf_algorithm pdf --qsm_algorithm rts --auto_yes --use_existing_masks"
+
+echo "[INFO] Collecting QSMxT results"
+if ls qsmxt_output/qsm/*.nii 1> /dev/null 2>&1; then
+    sudo gzip -f qsmxt_output/qsm/*.nii
+fi
+sudo mv qsmxt_output/qsm/*.nii.gz "recons/${ALGO_NAME}/${ALGO_NAME}.nii.gz"
+
+echo "[INFO] Deleting old outputs"
+sudo rm -rf qsmxt_output/
+
