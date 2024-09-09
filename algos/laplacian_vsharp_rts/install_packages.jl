0768eaf36da2fce2efc366aa86cc6a1758d7f3d8
Ashley Stewart <a.stewart.au@gmail.com>
2024-09-09 19:20:24 +1000

    Fix add JSON

--- algos/laplacian_vsharp_rts/install_packages.jl
+++ algos/laplacian_vsharp_rts/install_packages.jl
@@ -1,5 +1,5 @@
 using Pkg
 ENV["JULIA_PKG_PRECOMPILE_AUTO"]=0
-Pkg.add("JSON"))
+Pkg.add("JSON")
 Pkg.add(Pkg.PackageSpec(name="QSM", version=v"0.5.4"))
 Pkg.add(Pkg.PackageSpec(name="NIfTI", version=v"0.6.0"))
